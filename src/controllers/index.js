a7392b0d516b82a41a298d8648e820f9b0e9a915
LordOden <butter2002at@gmail.com>
2024-10-21 13:40:30 -0400

    added content from Oct18

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+// pull in our models. This will automatically load the index.js from that folder
+const models = require('../models');
+
 // There is no reason for the name here except an arbitary example
 // of updating the server data based on a client request.
 let name = 'unknown';
@@ -14,4 +17,36 @@ const hostPage1 = (req, res) => {
 
 const hostPage2 = (req, res) => {
   return res.sendFile(path.resolve(`${__dirname}/../../views/page2.html`));
+};
+
+const getName = (req, res) => {
+  return res.json({name});
+};
+
+const setName = (req, res) => {
+  if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
+    return res.status(400).json({ error: 'firstname, lastname and beds are all required' });
+  }
+  
+};
+const searchName = (req, res) => {
+  if (!req.query.name) {
+    return res.status(400).json({ error: 'Name is required to perform a search' });
+  }
+};
+
+const notFound = (req, res) => {
+  return res.status(404).sendFile(path.resolve(`${__dirname}/../../views/notFound.html`));
+
+};
+
+
+module.exports = {
+  index: hostIndex,
+  page1: hostPage1,
+  page2: hostPage2,
+  getName,
+  setName,
+  searchName,
+  notFound,
 };
--- src/router.js
+++ src/router.js
@@ -1,7 +1,16 @@
+//We are actually importing controllers/index.js
 const controllers = require('./controllers');
 
 const router = (app) => {
+    app.get('/page1', controllers.page1);
+    app.get('/page2', controllers.page2);
+    app.get('/getName', controllers.getName);
+    
+    app.get('/', controllers.index);
 
+    app.get('/*', controllers.notFound);
+
+    app.post('/setName', controllers.setName);
 };
 
 module.exports = router;
